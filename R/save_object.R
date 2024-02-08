a7794e1e70a75e0394bb9f19c71b33aee383d100
samuelwald <109024477+samuelwald@users.noreply.github.com>
2024-02-08 12:59:35 +0100

    Add files via upload

---
+++ R/save_object.R
@@ -0,0 +1,19 @@
+#' Save your file without overwriting it
+#' @param object_name Name of your object you want to save
+#' @param path_to_object Path where you want to store your object, needs to contain the name for your object
+#' @param overwrite TRUE for overwriting, default is FALSE
+
+
+#' @export
+
+save_object <- function(object_name, path_to_object, overwrite = FALSE){
+    if (file.exists(path_to_object) == FALSE){
+        saveRDS(object_name, path_to_object)
+    }
+    else if (file.exists(path_to_object) == TRUE & overwrite == TRUE){
+        saveRDS(object_name, path_to_object)
+    }
+    else {
+        print("Already saved")
+    }
+}
