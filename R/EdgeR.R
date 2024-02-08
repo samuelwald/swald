5370e58909f3b8abaff7619cb6ec426d4eace185
samuelwald <109024477+samuelwald@users.noreply.github.com>
2024-02-08 13:59:31 +0000

    New functions

--- R/EdgeR.R
+++ R/EdgeR.R
@@ -1,4 +1,5 @@
 #' Functions for analaysing single cell data with EdgeR
+#' 
 #' @param names Names of your clusters
 #' 
 #' @export 
@@ -21,6 +22,67 @@ build_contrasts_cluster <- function(names){
             else{
                 df[j, i] <- 1/-(length(rownames(df))-4)
             }
+        }
+    }
+    return(df)
+    
+}
+
+#' @param n_timepoints Number of timepoints
+
+#' @export 
+build_contrasts_timepoints <- function(names, n_timepoints){
+    rownames <- names
+    colnames <- list()
+    for (i in names){
+        colnames[[i]] <- i
+        colnames <- unique(unname(unlist(colnames)))
+    }
+    
+    df <- data.frame(matrix(ncol = length(colnames), nrow = length(rownames)))
+    rownames(df) <- rownames
+    colnames(df) <- colnames
+    for (i in colnames(df)){
+        for (j in rownames(df)){
+            if (i == j){
+                df[j,i] <- 1
+            }
+            else if (i != j & startsWith(i, gsub("_tp.*", "", j))){
+                df[j,i] <- -1/(n_timepoints -1)
+            }
+            else {
+                df[j,i] <- 0
+            }
+
+        }
+    }
+    return(df)
+    
+}
+
+
+#' @export 
+
+build_contrasts_cluster_timepoints <- function(names){
+    rownames <- names
+    colnames <- list()
+    for (i in names){
+        colnames[[i]] <- i
+        colnames <- unique(unname(unlist(colnames)))
+    }
+    
+    df <- data.frame(matrix(ncol = length(colnames), nrow = length(rownames)))
+    rownames(df) <- rownames
+    colnames(df) <- colnames
+    for (i in colnames(df)){
+        for (j in rownames(df)){
+            if (i == j){
+                df[j,i] <- 1
+            }
+            else {
+                df[j,i] <- -1/(nrow(df)-1)
+            }
+
         }
     }
     return(df)
